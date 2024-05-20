b40c276ea638046f91979322cbd87e103eb1a984
Bhavya Wahie <bhavyawahie@gmail.com>
2024-05-21 03:21:59 +0530

    review and product entites added

--- src/main/java/com/goodbuy/store/entity/Product.java
+++ src/main/java/com/goodbuy/store/entity/Product.java
@@ -1,2 +1,35 @@
-package com.goodbuy.store.entity;public class Product {
+package com.goodbuy.store.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="products")
+public class Product {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "userId")
+	private User user;
+	private String name;
+	private String brand;
+	private String category;
+	private String description;
+	private Double rating;
+	private Long numReviews;
+	private String image;
+	private Long price;
+	private Long countInStock;
 }
--- src/main/java/com/goodbuy/store/entity/Review.java
+++ src/main/java/com/goodbuy/store/entity/Review.java
@@ -1,2 +1,25 @@
-package com.goodbuy.store.entity;public class Review {
+package com.goodbuy.store.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "reviews")
+public class Review {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private Double rating;
+	private String comment;
+	@ManyToOne(fetch = FetchType.LAZY) // Many reviews can have one product
+	@JoinColumn(name = "productId") // Specify foreign key column
+	private Product product;
 }
