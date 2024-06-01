0ea56a6ac0cf3c6063db90eddb33c4dc49c05127
Bhavya Wahie <bhavyawahie@gmail.com>
2024-06-02 02:15:30 +0530

    Annotation for timestamp added createdAt Date-time field

--- src/main/java/com/goodbuy/store/entity/Order.java
+++ src/main/java/com/goodbuy/store/entity/Order.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.util.Date;
 import java.util.List;
@@ -70,4 +71,6 @@ public class Order {
 	private Boolean isDelivered = false;
 
 	private Date deliveredAt;
+	@CreationTimestamp
+	private Date createdAt;
 }
