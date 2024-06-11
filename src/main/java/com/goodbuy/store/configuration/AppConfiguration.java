c0fa27efc8f0bf40a9420dd70f546427fc396a7f
Bhavya Wahie <bhavyawahie@gmail.com>
2024-06-11 23:12:54 +0530

    cors config tweaked

--- src/main/java/com/goodbuy/store/configuration/AppConfiguration.java
+++ src/main/java/com/goodbuy/store/configuration/AppConfiguration.java
@@ -65,6 +65,7 @@ public class AppConfiguration implements WebMvcConfigurer {
 	}
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
+		registry.addMapping("/api/v1/**")
+				.allowedOrigins("https://lucent-sunshine-152090.netlify.app/").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
 	}
 }
