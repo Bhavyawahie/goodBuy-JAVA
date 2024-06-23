e8538addd095a0effd86f2c4b544d1bb35a2f63f
Bhavya Wahie <bhavyawahie@gmail.com>
2024-06-23 18:45:53 +0530

    jwt expiration values modified

--- src/main/java/com/goodbuy/store/utils/JwtService.java
+++ src/main/java/com/goodbuy/store/utils/JwtService.java
@@ -38,7 +38,7 @@ public class JwtService {
 				.setClaims(claims)
 				.setSubject(user.getUsername())
 				.setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis() * 1000 * 60 * 60 * 24 * 30))
+				.setExpiration(new Date(System.currentTimeMillis() * 1000 * 60 * 60 * 24))
 				.signWith(getSignInKey(), SignatureAlgorithm.HS256)
 				.compact();
 	}
